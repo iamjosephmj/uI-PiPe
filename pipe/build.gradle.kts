4a08e7f4489ccca0e0ab6ca533fcfa52fa1e1705
Joseph MJ <iamjosephmj@gmail.com>
2026-08-12 13:00:59 +0200

    chore: scaffold Gradle project with :pipe library module

---
+++ pipe/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+}
+
+android {
+    namespace = "tech.ssemaj.pipe"
+    compileSdk = 36
+    defaultConfig { minSdk = 35 }
+    buildFeatures { aidl = true }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+}
---
+++ pipe/src/test/java/tech/ssemaj/pipe/SmokeTest.kt
@@ -0,0 +1,8 @@
+package tech.ssemaj.pipe
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SmokeTest {
+    @Test fun gradleUnitTestsRun() { assertTrue(true) }
+}
